b5f8a4c781de0b6d9e7dcfa98075c93cadda9cf9
sidhu <sdupati4@gmail.com>
2026-09-09 20:31:11 +0530

    Allow Netlify frontend in CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -18,7 +18,10 @@ app = FastAPI(title="Notes App API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[
+    "http://localhost:5173",
+    "https://my-notessapp.netlify.app",
+],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
